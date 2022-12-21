387e96d23c17b7f4572ee161e0f8afaeefb6a743
Nur Efsan ALBAŞ <115089520+efsanalbas@users.noreply.github.com>
2022-12-21 19:42:19 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
-// service-worker.js
-self.addEventListener('install', function () {
-    console.log('Install!');
-});
-self.addEventListener("activate", event => {
-    console.log('Activate!');
-});
-self.addEventListener('fetch', function (event) {
-    console.log('Fetch!', event.request);
-});
+const CACHE ='https://efsanalbas.github.io/advancedProgramming/adamAsmaca.html'
+const FILES = ['https://efsanalbas.github.io/advancedProgramming/adamAsmaca.html']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
